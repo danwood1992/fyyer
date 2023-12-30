1169027f04127ced6e59b68927df02280662b04f
danwood1992 <gunnerwoody449@gmail.com>
2023-12-30 12:26:53 +0000

    Add demo script

--- demo_scripts/psycopg2.py
+++ demo_scripts/psycopg2.py
@@ -1,3 +1,34 @@
 import psycopg2
+import os
 
-connection = psycopg2.connect("dbname=postgres user=postgres")
+
+conn = psycopg2.connect(
+    dbname=os.environ.get('POSTGRES_DB'),
+    user=os.environ.get('POSTGRES_USER'),
+    password=os.environ.get('POSTGRES_PASSWORD'),
+    host=os.environ.get('POSTGRES_HOST'),
+    port=os.environ.get('POSTGRES_PORT')
+    )
+
+cursor = conn.cursor()
+
+## Open a cursor to perform database operations
+cur = conn.cursor()
+
+## drop any existing todos table
+cur.execute("DROP TABLE IF EXISTS todos;")
+
+## (re)create the todos table
+## (note: triple quotes allow multiline text in python)
+cur.execute("""
+  CREATE TABLE todos (
+    id serial PRIMARY KEY,
+    description VARCHAR NOT NULL
+  );
+""")
+
+## commit, so it does the executions on the db and persists in the db
+conn.commit()
+
+cur.close()
+conn.close()
