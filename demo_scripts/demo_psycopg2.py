8bfc116e0d44c92b9fee8e7c3972f85a4edab5a3
danwood1992 <gunnerwoody449@gmail.com>
2023-12-30 15:58:58 +0000

    Add db utils.

--- demo_scripts/demo_psycopg2.py
+++ demo_scripts/demo_psycopg2.py
@@ -1,34 +1,17 @@
-import psycopg2
-import os
+import demo_utils
+
+## create todo table
+
+demo_utils.create_table("todo", "id SERIAL PRIMARY KEY, task VARCHAR(255) NOT NULL, status BOOLEAN NOT NULL")
+
+demo_utils.insert_row("todo", "task, status", "'Finish this tutorial', FALSE")
 
 
-conn = psycopg2.connect(
-    dbname=os.environ.get('POSTGRES_DB'),
-    user=os.environ.get('POSTGRES_USER'),
-    password=os.environ.get('POSTGRES_PASSWORD'),
-    host=os.environ.get('POSTGRES_HOST'),
-    port=os.environ.get('POSTGRES_PORT')
-    )
 
-cursor = conn.cursor()
+demo_utils.fetch_all("todo")
 
-## Open a cursor to perform database operations
-cur = conn.cursor()
 
-## drop any existing todos table
-cur.execute("DROP TABLE IF EXISTS todos;")
 
-## (re)create the todos table
-## (note: triple quotes allow multiline text in python)
-cur.execute("""
-  CREATE TABLE todos (
-    id serial PRIMARY KEY,
-    description VARCHAR NOT NULL
-  );
-""")
 
-## commit, so it does the executions on the db and persists in the db
-conn.commit()
 
-cur.close()
-conn.close()
+
