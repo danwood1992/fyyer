0f53a40568f1e93b4b20733d7d152ceb8db016b2
danwood1992 <gunnerwoody449@gmail.com>
2024-01-01 21:29:06 +0000

    Using str method exercise

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fyyer:fyyer@fyyer-db:5432/fyyer'
+
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Person ID: {self.id}, name: {self.name}>'
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
-
 def index():
-    return 'Hello World!'
+    person = Person.query.first()
+    print(person)
+    if person:
+        return f"{person}"  # This will use __repr__ implicitly
+    else:
+        return 'No person found!'
 
-
+if __name__ == '__main__': 
+   app.run(host="0.0.0.0", port=5125, debug=True)
