74a88cf802361ed9233eeebc31855040252238fe
danwood1992 <gunnerwoody449@gmail.com>
2024-01-02 15:28:01 +0000

    Add create from the frontend

--- app.py
+++ app.py
@@ -9,13 +9,41 @@ def index():
     tasks = TodoItem.query.all()
     return render_template('index.html', users=users, tasks=tasks)
 
-@app.route('/seed/<string:name>')
-def seed_data(name):
+@app.route('/add-user', methods=['POST'])
+def add_user():
+    name = request.form.get('name')
+    user = User(name=name, email=f"{name.lower()}@mail.com")
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/add-task', methods=['POST'])
+def add_task():
+    description = request.form.get('description')
+    task = TodoItem(description=description)
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/add-user/<string:name>')
+def seed_user(name):
     try:
         user = User(name=name, email=f"{name.lower()}@mail.com")
         db.session.add(user)
         db.session.commit()
         return f"User {name} added successfully."
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+    
+@app.route('/add-task/<string:description>')
+def seed_task(description):
+    try:
+        task = TodoItem(description=description)
+        db.session.add(task)
+        db.session.commit()
+        return f"Task {description} added successfully."
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
